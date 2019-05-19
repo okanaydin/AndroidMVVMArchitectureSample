3e2041c9f1a37bfa1d3d910f5c867a11ba6de25b
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-05-20 01:35:57 +0300

    Add BaseViewModel
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

---
+++ app/src/main/java/com/okanaydin/androidmvvmarchitecturesample/base/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package com.okanaydin.androidmvvmarchitecturesample.base
+
+import androidx.lifecycle.ViewModel
+
+
+/**
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 2019-05-20 - 01:34          │
+└─────────────────────────────┘
+ */
+abstract class BaseViewModel: ViewModel(){
+
+}
