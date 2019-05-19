7c17d3d5c6d7126f8d583458be3aac57a2b4cb92
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-05-20 02:43:37 +0300

    Update PostListViewModel
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

--- app/src/main/java/com/okanaydin/androidmvvmarchitecturesample/ui/post/PostListViewModel.kt
+++ app/src/main/java/com/okanaydin/androidmvvmarchitecturesample/ui/post/PostListViewModel.kt
@@ -2,6 +2,9 @@ package com.okanaydin.androidmvvmarchitecturesample.ui.post
 
 import com.okanaydin.androidmvvmarchitecturesample.base.BaseViewModel
 import com.okanaydin.androidmvvmarchitecturesample.network.PostApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 
@@ -18,4 +21,39 @@ import javax.inject.Inject
 class PostListViewModel: BaseViewModel(){
     @Inject
     lateinit var postApi: PostApi
+
+    private lateinit var subscription: Disposable
+
+    init{
+        loadPosts()
+    }
+
+    private fun loadPosts(){
+        subscription = postApi.getPosts()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrievePostListStart() }
+            .doOnTerminate { onRetrievePostListFinish() }
+            .subscribe(
+                { onRetrievePostListSuccess() },
+                { onRetrievePostListError() }
+            )
+    }
+
+    private fun onRetrievePostListStart(){
+
+    }
+
+    private fun onRetrievePostListFinish(){
+
+    }
+
+    private fun onRetrievePostListSuccess(){
+
+    }
+
+    private fun onRetrievePostListError(){
+
+    }
+
 }
