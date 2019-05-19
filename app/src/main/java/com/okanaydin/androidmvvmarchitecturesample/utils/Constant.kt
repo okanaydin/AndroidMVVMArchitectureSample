b3b6c86f0b981b52a33b872f414a6d67196a20cd
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-05-20 01:45:29 +0300

    Create Constant for the base URL of the API
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

---
+++ app/src/main/java/com/okanaydin/androidmvvmarchitecturesample/utils/Constant.kt
@@ -0,0 +1,15 @@
+package com.okanaydin.androidmvvmarchitecturesample.utils
+
+
+/**
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 2019-05-20 - 01:44          │
+└─────────────────────────────┘
+ */
+
+/** The base URL of the API */
+const val BASE_URL: String = "https://jsonplaceholder.typicode.com"
